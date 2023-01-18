9afe3340587abc01cda8147c8b1394cc2da41ca3
Jarmo Hurri <jarmo.hurri@iki.fi>
2023-01-18 10:45:02 +0200

    * more intuitive assigment of game marks

--- TicTacToeApp.java
+++ TicTacToeApp.java
@@ -18,12 +18,9 @@ public class TicTacToeApp
   public static void main (String[] args)
   {
     TicTacToe ttt = new TicTacToe ();
-    ttt.marks [0][0] = Mark.Cross;
-    ttt.marks [1][0] = Mark.Nought;
-    ttt.marks [2][0] = Mark.Cross;
-    ttt.marks [1][1] = Mark.Nought;
-    ttt.marks [0][2] = Mark.Cross;
-    ttt.marks [1][2] = Mark.Nought;
+    ttt.marks = new Mark[][] {{Mark.Cross, Mark.Nought, Mark.Cross},
+                              {Mark.Nought, Mark.Cross, Mark.Nought},
+                              {Mark.Nought, Mark.Nought, Mark.Cross}};
     System.out.println (ttt.gameOver ());
   }
 }
