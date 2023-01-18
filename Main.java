5aada3e66bffbe97e2fc22a85f3d25fed6e27608
Jarmo Hurri <jarmo.hurri@iki.fi>
2023-01-18 10:47:29 +0200

    * change name of controller class to Main

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-public class TicTacToeApp
+public class Main
 {
   public static void main (String[] args)
   {
